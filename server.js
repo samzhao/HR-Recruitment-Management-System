edc9bb8163da7a41aa7be79a4d906a4e54257ba5
Sam Zhao <zhao6518@gmail.com>
2016-04-16 03:11:16 -0700

    Update express server

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(req, res, next) {
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
