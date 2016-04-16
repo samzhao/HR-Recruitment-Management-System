78e94338961a7640aa9e2ae7cc0f022df1f76f4c
Sam Zhao <zhao6518@gmail.com>
2016-04-16 01:47:54 -0700

    Live version

--- src/components/Icon.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-require('ionicons/dist/scss/ionicons.scss')
-
-export default ({ name, component }) => (
-  React.createElement((component || 'span'), {
-    className: 'icon ' + (name.indexOf('ion-') === 0 ? name : `ion-${name}`)
-  })
-)
