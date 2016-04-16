c5773773ee5e80b6ded1a2b14c4c5339e08521a8
Sam Zhao <zhao6518@gmail.com>
2016-04-16 15:02:43 -0700

    Redirect everything

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use('/', express.static(root));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(fallback('index.html', { root: root }))
+app.get('*', function(req, res) {
+  res.sendFile(path.join(root, 'index.html'))
+})
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
