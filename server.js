7dad471b5dd5d74d428c652f82e6922a950ff6e4
Sam Zhao <zhao6518@gmail.com>
2016-04-16 03:20:59 -0700

    Add history api fallback

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var fallback = require('express-history-api-fallback')
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
+var root = __dirname + '/dist'
+
+app.use('/', express.static(root));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+app.use(fallback('index.html', { root: root }))
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
